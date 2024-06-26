0d81d927c4b14901c7032c6615f0dbe230f00c80
Caitlin Mothes <ccmothes@gmail.com>
2024-06-26 15:29:05 -0600

    address r data issue with terra objects

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,16 +1,16 @@
 ## code to prepare `DATASET` dataset goes here
 library(terra)
 
-corr <- rast('data-raw/current_surface.tif')
-suit <- rast('data-raw/suitability_surface.tif')
-resist <- rast("data-raw/resistance_surface.tif")
-corridor <- rast("data-raw/corridor.tif")
+corr <- wrap(rast('data-raw/current_surface.tif'))
+suit <- wrap(rast('data-raw/suitability_surface.tif'))
+resist <- wrap(rast("data-raw/resistance_surface.tif"))
+corridor <- wrap(rast("data-raw/corridor.tif"))
 load("data-raw/patch_priorEx.RData")
 
 # change rasterlayers to terra
-patch$qwa <- rast(patch$qwa)
-patch$btwn <- rast(patch$btwn)
-patch$dECA <- rast(patch$dECA)
+patch$qwa <- wrap(rast(patch$qwa))
+patch$btwn <- wrap(rast(patch$btwn))
+patch$dECA <- wrap(rast(patch$dECA))
 
 
 usethis::use_data(corr, suit, resist, patch, corridor, overwrite = TRUE)
